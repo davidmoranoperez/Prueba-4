e9529dfa0afc850a1a26785c054552187b8b3049
david morano <davidmorano91@gmail.com>
2018-05-21 14:06:49 +0200

    cambios de comentarios realizados

--- src/videojuego/Selenita.java
+++ src/videojuego/Selenita.java
@@ -1,15 +1,25 @@
 package videojuego;
 
+/**
+ *
+ * @author David Morano Pérez
+ */
 public class Selenita extends Extraterrestre implements DominarPlaneta {
 
     private String armamento;
 
+    /**
+     * Lanza el método atacar terricola de su clase padre
+     */
     public void atacarTerricola() {
         super.atacarTerricola();
         System.out.println("Aniquilar terrícola");
         dispararArma();
     }
 
+    /**
+     * Muestra un mensaje con el ruido del arma
+     */
     public void dispararArma() {
         if (armamento.equals("cañon láser")) {
             System.out.println("BOUMMM");
@@ -18,24 +28,44 @@ public class Selenita extends Extraterrestre implements DominarPlaneta {
         }
     }
 
+    /**
+     * Llama a su clase padre para crear el armamento.
+     * @param armamento
+     */
     public Selenita(String armamento) {
         super(2, 1, "Azul");
         this.armamento = armamento;
     }
 
+    /**
+     *
+     * @param armamento
+     */
     public void asignarArmamento(String armamento) {
         this.armamento = armamento;
     }
 
+    /**
+     *
+     * @param planeta
+     */
     public void proclamarGobierno(String planeta) {
         System.out.println("Eliminar gobernantes del planeta:" + planeta);
         System.out.println("Gobernamos sobre el planeta " + planeta);
     }
 
+    /**
+     *
+     * @param planeta
+     */
     public void aniquilarPoblacion(String planeta) {
         System.out.println("Utilización de armas nucleares sobre el planeta " + planeta);
     }
 
+    /**
+     *
+     * @param planeta
+     */
     public void controlarRecursos(String planeta) {
     }
 }
